86fe08f094429eb999f9a87c24b754dfd74d1774
Miguel Orts <cesur.s320598@cesurformacion.com>
2023-04-20 20:18:52 +0200

    reestructurado el código

--- src/Proyecto/Login.java
+++ src/Proyecto/Login.java
@@ -3,9 +3,21 @@ package Proyecto;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 public class Login extends JFrame {
+
+    // INICIALIZAMOS TODAS LAS CLASES
+    private JPanel panel;
+    private JLabel label;
+    private JLabel label2;
+    private JTextField nombreUsuario;
+    private JPasswordField contraseña;
+    private JButton loginButton;
+    private JButton registerButton;
+
     public Login() { //Constructor del Frame
 
         // FRAME
@@ -20,65 +32,112 @@ public class Login extends JFrame {
     }
     private void iniciarComponentes(){ //Método para iniciar y agregar el Panel y todos sus componentes en el Frame
 
+        colocarPanel();
+        colocarLabels();
+        colocarCamposTexto();
+        colocarBotones();
+
+        setVisible(true); /*Solo hacemos visible el Panel para que se vean todos los elementos, puesto que si hiciéramos visible
+        el Frame opacaría el Panel y no veríamos todos los widgets que contiene*/
+    }
+
+    private void colocarPanel(){
+
         // PANEL (CONTENDRÁ TODOS LOS WIDGETS)
 
-        JPanel panel = new JPanel();
+        panel = new JPanel(); // Creamos Panel
         panel.setLayout(null); //Deshabilitamos el diseño por defecto para poder posicionar los widgets en el panel libremente
         this.getContentPane().add(panel); //Con "this" nos referimos al Frame, "getContent" su contenido y add para añadir el Panel
         panel.setBackground(Color.BLACK); //Color de fondo del Panel
 
         Border border = BorderFactory.createLineBorder(Color.green, 4); //Diseñamos el borde del Panel (color y grosor)
         panel.setBorder(border); //Añadimos el borde al Panel
+    }
+    private void colocarLabels(){
 
         // IMÁGEN LOGO
 
-        URL urllogo = Proyecto.Login.class.getResource("Imagenes/Green-Devil-988x1024.png"); 
+        URL urllogo = Proyecto.Login.class.getResource("Imagenes/Green-Devil-988x1024.png");
         ImageIcon image = new ImageIcon(urllogo);
         Icon icon = new ImageIcon(image.getImage().getScaledInstance(260, 270, Image.SCALE_SMOOTH));
 
         // LABELS (TEXTO Y LOGO)
 
-        JLabel label = new JLabel(); //Label que contendrá el logo de la página
+        label = new JLabel(); //Label que contendrá el logo de la página
         label.setIcon(icon); //Le añadimos la imagen del logo
         label.setBounds(230, 70, 260, 270); //Establecemos su ubicación en el Panel
         panel.add(label); //Añadimos el label al Panel
 
-        JLabel label2 = new JLabel(); //Label del texto de bienvenida
+        label2 = new JLabel(); //Label del texto de bienvenida
         label2.setText("¡Bienvenido a Green Devil MusicStore!"); //Creamos el texto
         label2.setForeground(new Color(0x00FF00)); //Le añadimos un color al texto
         label2.setFont(new Font("Eras Bold ITC", Font.PLAIN,20)); //Le asignamos la fuente y el tamaño de la letra al texto
         label2.setBounds(160, 20, 400, 40);
         panel.add(label2);
+    }
+    private void colocarCamposTexto(){
 
         // CAMPOS DE TEXTO PARA INICIAR SESIÓN (USUARIO Y CONTRASEÑA)
 
-        JTextField nombreUsuario = new JTextField("Nombre de usuario..."); //Campo de texto para el nombre de usuario
+        nombreUsuario = new JTextField("Nombre de usuario..."); //Campo de texto para el nombre de usuario
         nombreUsuario.setBounds(170, 350, 340, 40);
         nombreUsuario.setForeground(new Color(0x049B04));
         nombreUsuario.setHorizontalAlignment(JTextField.CENTER); //Alineamos el texto en el centro del campo de texto
         nombreUsuario.setBorder(BorderFactory.createLineBorder(new Color(0x049B04), 3));
         panel.add(nombreUsuario);
 
-        JPasswordField contraseña = new JPasswordField("Contraseña..."); //Campo de texto para la contraseña
+        contraseña = new JPasswordField("Contraseña..."); //Campo de texto para la contraseña
         contraseña.setBounds(170, 410, 340, 40);
         contraseña.setForeground(new Color(0x049B04));
         contraseña.setHorizontalAlignment(JTextField.CENTER); //Alineamos el texto en el centro del campo de texto
         contraseña.setBorder(BorderFactory.createLineBorder(new Color(0x049B04), 3)); //Color bordes del campo de texto
         panel.add(contraseña);
+    }
+
+    private void colocarBotones(){
 
         // BOTONES DE INICIO DE SESIÓN Y REGISTRO
 
-        JButton loginButton = new JButton("Iniciar Sesión"); //Botón de Inicio de Sesión
+        loginButton = new JButton("Iniciar Sesión"); //Botón de Inicio de Sesión
         loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         loginButton.setBounds(275, 480, 130, 25);
         panel.add(loginButton);
 
-        JButton registerButton = new JButton("Registrarse"); //Botón de Registro
+        registerButton = new JButton("Registrarse"); //Botón de Registro
         registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         registerButton.setBounds(560, 520, 100, 25);
         panel.add(registerButton);
 
-        setVisible(true); /*Solo hacemos visible el Panel para que se vean todos los elementos, puesto que si hiciéramos visible
-        el Frame opacaría el Panel y no veríamos todos los widgets que contiene*/
+        // EVENTO ACTIONLISTENER (UNA VEZ INICIADO SESIÓN ACCEDEMOS A LA VENTANA PRINCIPAL DE LA WEB)
+        ActionListener oyenteDeAccion = new ActionListener() { //Agregando evento de tipo ActionListener
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String [] Usuarios={"admin", "miguel"};
+                String [] Claves = new String[2];
+                Claves[0]="1234";
+                Claves[1]="12345";
+
+                String usuario = nombreUsuario.getText();
+                String password= contraseña.getText();
+                boolean mensaje = false;
+                for(int i = 0; i < Usuarios.length; i++){
+                    if(Usuarios[i].equals(usuario)&&Claves[i].equals(password)){
+                        mensaje=true;            }
+                }
+                if(mensaje){
+                    Principal principal = new Principal();
+                    principal.setVisible(true);
+
+                }else {
+                    JOptionPane.showMessageDialog(null, "Usario o contraseña incorrectos");
+                    nombreUsuario.setText("");
+                    contraseña.setText("");
+                }
+            }
+        };
+        loginButton.addActionListener(oyenteDeAccion); //Le agreamos al botón de Iniciar Sesión el evento ActionListener
+
     }
+
+
 }
