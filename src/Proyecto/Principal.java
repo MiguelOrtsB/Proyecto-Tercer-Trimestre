b23bf1edf090023fb0e53c4d5952e6e178b4aaa0
Miguel Orts <cesur.s320598@cesurformacion.com>
2023-04-29 03:00:00 +0200

    cambios

--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -11,6 +11,8 @@ public class Principal extends JFrame {
     private JPanel panelPrincipal;
     private JMenuBar navbar;
     private JMenu menu1;
+    private JMenu menu11;
+    private JMenu menu12;
 
     public Principal(){
 
@@ -43,7 +45,6 @@ public class Principal extends JFrame {
         panelPrincipal.setBackground(Color.WHITE);
         Border border = BorderFactory.createLineBorder(new Color(0x049B04), 4); //Diseñamos el borde del Panel (color y grosor)
         panelPrincipal.setBorder(border);
-
     }
 
     private void colocarLabelsPrincipal(){
@@ -90,7 +91,19 @@ public class Principal extends JFrame {
         ImageIcon imagen10 = new ImageIcon(urlLogo10);
         Icon icono10 = new ImageIcon(imagen10.getImage().getScaledInstance(250, 80, Image.SCALE_SMOOTH));
 
-        //LABELS PARA IMÁGENES
+        URL urlLogo11 = Proyecto.Principal.class.getResource("Imagenes/MXR_Logo_G.png");
+        ImageIcon imagen11 = new ImageIcon(urlLogo11);
+        Icon icono11 = new ImageIcon(imagen11.getImage().getScaledInstance(255, 100, Image.SCALE_SMOOTH));
+
+        URL urlLogo12 = Proyecto.Principal.class.getResource("Imagenes/Vox-logo-Warranty-DAR-WEB.png");
+        ImageIcon imagen12 = new ImageIcon(urlLogo12);
+        Icon icono12 = new ImageIcon(imagen12.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+
+        URL urlLogo13 = Proyecto.Principal.class.getResource("Imagenes/Yamaha_logo_dark.svg.png");
+        ImageIcon imagen13 = new ImageIcon(urlLogo13);
+        Icon icono13 = new ImageIcon(imagen13.getImage().getScaledInstance(240, 50, Image.SCALE_SMOOTH));
+
+        //LABELS PARA LAS IMÁGENES Y TEXTOS
 
         JLabel imagenLogo = new JLabel();
         imagenLogo.setIcon(icono);
@@ -141,6 +154,39 @@ public class Principal extends JFrame {
         imagenLogo10.setIcon(icono10);
         imagenLogo10.setBounds(925, 526, 300, 150);
         panelPrincipal.add(imagenLogo10);
+
+        JLabel imagenLogo11 = new JLabel();
+        imagenLogo11.setIcon(icono11);
+        imagenLogo11.setBounds(328, 665, 300, 150);
+        panelPrincipal.add(imagenLogo11);
+
+        JLabel imagenLogo12 = new JLabel();
+        imagenLogo12.setIcon(icono12);
+        imagenLogo12.setBounds(655, 695, 300, 150);
+        panelPrincipal.add(imagenLogo12);
+
+        JLabel imagenLogo13 = new JLabel();
+        imagenLogo13.setIcon(icono13);
+        imagenLogo13.setBounds(930, 670, 300, 150);
+        panelPrincipal.add(imagenLogo13);
+
+        JLabel music = new JLabel("MUSIC");
+        music.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,120));
+        music.setBounds(220, 25, 400, 100);
+        music.setForeground(new Color(0x049B04));
+        panelPrincipal.add(music);
+
+        JLabel store = new JLabel("STORE");
+        store.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,120));
+        store.setBounds(900, 25, 400, 100);
+        store.setForeground(new Color(0x049B04));
+        panelPrincipal.add(store);
+
+        JLabel nuestrasMarcas = new JLabel("NUESTRAS MARCAS:");
+        nuestrasMarcas.setFont(new Font("Impact", Font.ROMAN_BASELINE,20));
+        nuestrasMarcas.setBounds(673, 165, 200, 100);
+        nuestrasMarcas.setForeground(Color.BLACK);
+        panelPrincipal.add(nuestrasMarcas);
     }
 
     private void colocarMenuBar(){
@@ -153,6 +199,8 @@ public class Principal extends JFrame {
         navbar.setBounds(4, 145, 1528, 50); //Tamaño y posición del MenuBar
         panelPrincipal.add(navbar); //La añadimos al PANEL
 
+        // OPCIONES DEL MENU BAR (MENÚ, CONTACTO, INFO)
+
         menu1 = new JMenu("Menú"); //Añadimos el Menu1 que va encima del MenuBar y que abrirá el desplegable de opciones
         menu1.setFont(new Font("Cinzel", Font.PLAIN,18)); //Le aplicamos tipo de fuente, color y tamaño
         menu1.setForeground(Color.WHITE); //Color de la fuente
@@ -161,6 +209,24 @@ public class Principal extends JFrame {
         menu1.setRolloverEnabled(true);
         menu1.addMouseListener(mouseAction); //Añadimos los eventos de ratón de más abajo al Menu1
         navbar.add(menu1); //Añadimos el Menu al MenuBar
+
+        menu11 = new JMenu("Contacto"); //Añadimos el Menu1 que va encima del MenuBar y que abrirá el desplegable de opciones
+        menu11.setFont(new Font("Cinzel", Font.PLAIN,18)); //Le aplicamos tipo de fuente, color y tamaño
+        menu11.setForeground(Color.WHITE); //Color de la fuente
+        menu11.setCursor(new Cursor(Cursor.HAND_CURSOR)); //Cambia el tipo de cursor cuando nos posamos encima
+        menu11.setRolloverEnabled(true);
+        menu11.addMouseListener(mouseAction); //Añadimos los eventos de ratón de más abajo al Menu11
+        navbar.add(menu11); //Añadimos el Menu al MenuBar
+
+        menu12 = new JMenu("Info"); //Añadimos el Menu1 que va encima del MenuBar y que abrirá el desplegable de opciones
+        menu12.setFont(new Font("Cinzel", Font.PLAIN,18)); //Le aplicamos tipo de fuente, color y tamaño
+        menu12.setForeground(Color.WHITE); //Color de la fuente
+        menu12.setCursor(new Cursor(Cursor.HAND_CURSOR)); //Cambia el tipo de cursor cuando nos posamos encima
+        menu12.setRolloverEnabled(true);
+        menu12.addMouseListener(mouseAction); //Añadimos los eventos de ratón de más abajo al Menu12
+        navbar.add(menu12); //Añadimos el Menu al MenuBar
+
+        // SUBOPCIONES DENTRO DE MENÚ
 
         JMenu menu2 = new JMenu("Guitarras"); //Este menu2 irá dentro del menu1, y se activará al hacer click en menu1
         menu2.setFont(new Font("Cinzel", Font.PLAIN,20));
@@ -181,17 +247,49 @@ public class Principal extends JFrame {
         menu4.setFont(new Font("Cinzel", Font.PLAIN,20));
         menu4.setCursor(new Cursor(Cursor.HAND_CURSOR));
         menu1.add(menu4);
+
+        // SUBOPCIONES DENTRO DE CONTACTO
+
+        JMenu menu6 = new JMenu("Teléfono");
+        menu6.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu6.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        menu11.add(menu6);
+
+        JMenu menu7 = new JMenu("Correo");
+        menu7.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu7.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        menu11.add(menu7);
+
+        JMenu menu8 = new JMenu("Dirección");
+        menu8.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu8.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        menu11.add(menu8);
+
+        // RESULTADOS PARA LA OPCIÓN DE "CONTACTO"
+
+        JMenuItem telf = new JMenuItem("971809097");
+        telf.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu6.add(telf);
+
+        JMenuItem correo = new JMenuItem("greendevilmusicstore@gmail.es");
+        telf.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu7.add(correo);
+
+        JMenuItem direccion = new JMenuItem("C/ Barón de Pinopar nº19");
+        telf.setFont(new Font("Cinzel", Font.PLAIN,20));
+        menu8.add(direccion);
     }
 
     // EVENTOS DE RATÓN (CUANDO PASAMOS CON EL RATÓN POR ENCIMA DEL MENÚ CAMBIA DE COLOR)
 
     private final MouseListener mouseAction = new MouseAdapter(){
+
         @Override
         public void mouseEntered(MouseEvent e) {
             menu1 = (JMenu) e.getSource(); //is this implementation correct ?
             menu1.setOpaque(true);
             menu1.setSelected(true);
-            menu1.setBackground(Color.BLUE);
+            menu1.setBackground(Color.BLACK);
         };
 
         @Override
@@ -205,4 +303,6 @@ public class Principal extends JFrame {
     public static void main(String[] args) {
         Principal principal = new Principal();
     }
+
+
 }
