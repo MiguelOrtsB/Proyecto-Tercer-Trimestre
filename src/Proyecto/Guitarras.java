7f84328c2b657a843d5a3f2c614fa04b557da348
Miguel Orts <cesur.s320598@cesurformacion.com>
2023-05-03 12:10:22 +0200

    MODIFICACIONES

--- src/Proyecto/Guitarras.java
+++ src/Proyecto/Guitarras.java
@@ -15,7 +15,13 @@ public class Guitarras extends JFrame{
 
 
     public Guitarras(){
-        setExtendedState(MAXIMIZED_BOTH);
+
+        // FRAME
+
+        //setExtendedState(MAXIMIZED_BOTH);
+        setSize(1280, 720);
+        setLocationRelativeTo(null);
+        setResizable(false);
         setTitle("\uD834\uDD1E Green Devil MusicStore \uD834\uDD1E"); // Título del Frame
         Image iconImage = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Imagenes/Green-Devil-988x1024.png"));
         setIconImage(iconImage); // Establecemos el icono de la ventana
@@ -52,142 +58,162 @@ public class Guitarras extends JFrame{
 
         URL urlLogo = Proyecto.Login.class.getResource("Imagenes/Green-Devil-988x1024.png");
         ImageIcon image = new ImageIcon(urlLogo);
-        Icon icon = new ImageIcon(image.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH));
+        Icon icon = new ImageIcon(image.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
         labelLogo = new JLabel(); //Label que contendrá el logo de la página
         labelLogo.setIcon(icon); //Le añadimos la imagen del logo
-        labelLogo.setBounds(30, 16, 160, 120); //Establecemos su ubicación en el Panel
+        labelLogo.setBounds(30, 5, 160, 120); //Establecemos su ubicación en el Panel
         panelGuitarras.add(labelLogo); //Añadimos el label al Panel
 
         // GUITARRA 1 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra = Proyecto.Login.class.getResource("Imagenes/Gibson_LP_Classic.png");
         ImageIcon imagenGuitarra = new ImageIcon(urlGuitarra);
-        Icon iconGuitarra = new ImageIcon(imagenGuitarra.getImage().getScaledInstance(150, 300, Image.SCALE_SMOOTH));
+        Icon iconGuitarra = new ImageIcon(imagenGuitarra.getImage().getScaledInstance(110, 220, Image.SCALE_SMOOTH));
         JLabel guitarra1 = new JLabel();
         guitarra1.setIcon(iconGuitarra);
-        guitarra1.setBounds(50, 170, 150, 300);
+        guitarra1.setBounds(72, 95, 150, 300);
         panelGuitarras.add(guitarra1);
         JLabel guit1 = new JLabel("Gibson Slash Les Paul Standard");
-        guit1.setBounds(35, 460, 300, 50);
+        guit1.setBounds(35, 340, 300, 50);
         panelGuitarras.add(guit1);
         JLabel prec1= new JLabel("2699€");
-        prec1.setBounds(55, 484, 50, 50);
+        prec1.setBounds(55, 363, 50, 50);
         prec1.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec1);
         JButton btn1 = new JButton("Comprar");
-        btn1.setBounds(115, 500, 85, 20);
+        btn1.setBounds(115, 380, 85, 20);
         panelGuitarras.add(btn1);
 
         // GUITARRA 2 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra2 = Proyecto.Login.class.getResource("Imagenes/2916352580_jac_ins_frt_1_rr.png");
         ImageIcon imagenGuitarra2 = new ImageIcon(urlGuitarra2);
-        Icon iconGuitarra2 = new ImageIcon(imagenGuitarra2.getImage().getScaledInstance(100, 290, Image.SCALE_SMOOTH));
+        Icon iconGuitarra2 = new ImageIcon(imagenGuitarra2.getImage().getScaledInstance(75, 215, Image.SCALE_SMOOTH));
         JLabel guitarra2 = new JLabel();
         guitarra2.setIcon(iconGuitarra2);
-        guitarra2.setBounds(320, 170, 150, 300);
+        guitarra2.setBounds(320, 92, 150, 300);
         panelGuitarras.add(guitarra2);
         JLabel guit2 = new JLabel("Jackson SL3X DX");
-        guit2.setBounds(320, 458, 300, 50);
+        guit2.setBounds(310, 339, 300, 50);
         panelGuitarras.add(guit2);
         JLabel prec2 = new JLabel("719€");
-        prec2.setBounds(300, 482, 50, 50);
+        prec2.setBounds(300, 363, 50, 50);
         prec2.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec2);
         JButton btn2 = new JButton("Comprar");
-        btn2.setBounds(350, 498, 85, 20);
+        btn2.setBounds(350, 380, 85, 20);
         panelGuitarras.add(btn2);
 
         // GUITARRA 3 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra3 = Proyecto.Login.class.getResource("Imagenes/fender-jv-modified-telecaster-custom-60.jpg");
         ImageIcon imagenGuitarra3 = new ImageIcon(urlGuitarra3);
-        Icon iconGuitarra3 = new ImageIcon(imagenGuitarra3.getImage().getScaledInstance(220, 290, Image.SCALE_SMOOTH));
+        Icon iconGuitarra3 = new ImageIcon(imagenGuitarra3.getImage().getScaledInstance(165, 215, Image.SCALE_SMOOTH));
         JLabel guitarra3 = new JLabel();
         guitarra3.setIcon(iconGuitarra3);
-        guitarra3.setBounds(520, 166, 170, 300);
+        guitarra3.setBounds(520, 94, 170, 300);
         panelGuitarras.add(guitarra3);
         JLabel guit3 = new JLabel("Fender Telecaster Custom 60");
-        guit3.setBounds(550, 457, 300, 50);
+        guit3.setBounds(523, 338, 300, 50);
         panelGuitarras.add(guit3);
         JLabel prec3 = new JLabel("2599€");
-        prec3.setBounds(560, 482, 50, 50);
+        prec3.setBounds(535, 363, 50, 50);
         prec3.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec3);
         JButton btn3 = new JButton("Comprar");
-        btn3.setBounds(620, 498, 85, 20);
+        btn3.setBounds(595, 380, 85, 20);
         panelGuitarras.add(btn3);
 
         // GUITARRA 4 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra4 = Proyecto.Login.class.getResource("Imagenes/evh-striped-series-frankie-relic.jpg");
         ImageIcon imagenGuitarra4 = new ImageIcon(urlGuitarra4);
-        Icon iconGuitarra4 = new ImageIcon(imagenGuitarra4.getImage().getScaledInstance(220, 290, Image.SCALE_SMOOTH));
+        Icon iconGuitarra4 = new ImageIcon(imagenGuitarra4.getImage().getScaledInstance(162, 212, Image.SCALE_SMOOTH));
         JLabel guitarra4 = new JLabel();
         guitarra4.setIcon(iconGuitarra4);
-        guitarra4.setBounds(780, 166, 170, 300);
+        guitarra4.setBounds(780, 95, 170, 300);
         panelGuitarras.add(guitarra4);
         JLabel guit4 = new JLabel("EVH Striped Series");
-        guit4.setBounds(840, 455, 300, 50);
+        guit4.setBounds(810, 338, 300, 50);
         panelGuitarras.add(guit4);
         JLabel prec4 = new JLabel("1359€");
-        prec4.setBounds(820, 480, 50, 50);
+        prec4.setBounds(790, 363, 50, 50);
         prec4.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec4);
         JButton btn4 = new JButton("Comprar");
-        btn4.setBounds(880, 496, 85, 20);
+        btn4.setBounds(850, 380, 85, 20);
         panelGuitarras.add(btn4);
 
         // GUITARRA 5 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra5 = Proyecto.Login.class.getResource("Imagenes/20230403211826-XF1.6FRFBR20-20FRONT20VERTICAL-394x1024.png");
         ImageIcon imagenGuitarra5 = new ImageIcon(urlGuitarra5);
-        Icon iconGuitarra5 = new ImageIcon(imagenGuitarra5.getImage().getScaledInstance(140, 310, Image.SCALE_SMOOTH));
+        Icon iconGuitarra5 = new ImageIcon(imagenGuitarra5.getImage().getScaledInstance(95, 230, Image.SCALE_SMOOTH));
         JLabel guitarra5 = new JLabel();
         guitarra5.setIcon(iconGuitarra5);
-        guitarra5.setBounds(1080, 170, 170, 300);
+        guitarra5.setBounds(1073, 95, 170, 300);
         panelGuitarras.add(guitarra5);
         JLabel guit5 = new JLabel("Ibanez XPTB720");
-        guit5.setBounds(1105, 454, 300, 50);
+        guit5.setBounds(1078, 338, 300, 50);
         panelGuitarras.add(guit5);
         JLabel prec5 = new JLabel("1299€");
-        prec5.setBounds(1080, 479, 50, 50);
+        prec5.setBounds(1055, 363, 50, 50);
         prec5.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec5);
         JButton btn5 = new JButton("Comprar");
-        btn5.setBounds(1140, 495, 85, 20);
+        btn5.setBounds(1115, 380, 85, 20);
         panelGuitarras.add(btn5);
 
         // GUITARRA 6 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
 
         URL urlGuitarra6 = Proyecto.Login.class.getResource("Imagenes/gibson_exploreran.png");
         ImageIcon imagenGuitarra6 = new ImageIcon(urlGuitarra6);
-        Icon iconGuitarra6 = new ImageIcon(imagenGuitarra6.getImage().getScaledInstance(110, 290, Image.SCALE_SMOOTH));
+        Icon iconGuitarra6 = new ImageIcon(imagenGuitarra6.getImage().getScaledInstance(85, 210, Image.SCALE_SMOOTH));
         JLabel guitarra6 = new JLabel();
         guitarra6.setIcon(iconGuitarra6);
-        guitarra6.setBounds(1350, 170, 170, 300);
+        guitarra6.setBounds(77, 370, 170, 300);
         panelGuitarras.add(guitarra6);
         JLabel guit6 = new JLabel("Gibson Explorer Antique Natural");
-        guit6.setBounds(1310, 455, 300, 50);
+        guit6.setBounds(35, 610, 300, 50);
         panelGuitarras.add(guit6);
         JLabel prec6 = new JLabel("1899€");
-        prec6.setBounds(1330, 480, 50, 50);
+        prec6.setBounds(55, 631, 50, 50);
         prec6.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
         panelGuitarras.add(prec6);
         JButton btn6 = new JButton("Comprar");
-        btn6.setBounds(1390, 498, 85, 20);
+        btn6.setBounds(350, 648, 85, 20);
         panelGuitarras.add(btn6);
+
+        // GUITARRA 7 (CON SU NOMBRE, IMÁGEN, PRECIO, ETC)
+
+        URL urlGuitarra7 = Proyecto.Login.class.getResource("Imagenes/yamaha-yamaha-f325d-acoustic-guitar.png");
+        ImageIcon imagenGuitarra7 = new ImageIcon(urlGuitarra7);
+        Icon iconGuitarra7 = new ImageIcon(imagenGuitarra7.getImage().getScaledInstance(95, 210, Image.SCALE_SMOOTH));
+        JLabel guitarra7 = new JLabel();
+        guitarra7.setIcon(iconGuitarra7);
+        guitarra7.setBounds(320, 370, 170, 300);
+        panelGuitarras.add(guitarra7);
+        JLabel guit7 = new JLabel("Yamaha F325 Acoustic");
+        guit7.setBounds(300, 610, 300, 50);
+        panelGuitarras.add(guit7);
+        JLabel prec7 = new JLabel("169€");
+        prec7.setBounds(300, 631, 50, 50);
+        prec7.setFont(new Font("Source Sans Pro Black", Font.PLAIN, 18));
+        panelGuitarras.add(prec7);
+        JButton btn7 = new JButton("Comprar");
+        btn7.setBounds(115, 648, 85, 20);
+        panelGuitarras.add(btn7);
 
         // TEXTO / TÍTULO DE LA VENTANA
 
         JLabel music = new JLabel("GUITARRAS");
-        music.setFont(new Font("Impact", Font.ROMAN_BASELINE,100));
-        music.setBounds(560, 20, 500, 100);
+        music.setFont(new Font("Impact", Font.ROMAN_BASELINE,70));
+        music.setBounds(480, 10, 500, 100);
         music.setForeground(new Color(0x049B04));
         panelGuitarras.add(music);
 
         JMenuBar linea = new JMenuBar();
-        linea.setBounds(4, 150, 1528, 6);
+        linea.setBounds(4, 120, 1518, 6);
         linea.setBackground(new Color(0x049B04));
         panelGuitarras.add(linea);
     }
@@ -197,7 +223,7 @@ public class Guitarras extends JFrame{
         atras = new JButton("↶ Atrás");
         atras.setFont(new Font("Dialog", Font.PLAIN, 18));
         atras.setAlignmentX(Component.CENTER_ALIGNMENT);
-        atras.setBounds(1410, 60, 100, 35);
+        atras.setBounds(1130, 45, 100, 35);
         panelGuitarras.add(atras);
 
         ActionListener accederRegister = new ActionListener() {
--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -13,10 +13,16 @@ public class Principal extends JFrame {
     private JMenu menu1;
     private JMenu menu11;
     private JMenu menu12;
+    private Dimension dim;
 
     public Principal(){
 
-        setExtendedState(MAXIMIZED_BOTH); //La ventana ocupa todoo el ancho de la pantalla
+        // FRAME
+
+        setSize(1280, 720);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        //setExtendedState(MAXIMIZED_BOTH); //La ventana ocupa todoo el ancho de la pantalla
         setTitle("\uD834\uDD1E Green Devil MusicStore \uD834\uDD1E"); // Título del Frame
         Image iconImage = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Imagenes/Green-Devil-988x1024.png"));
         setIconImage(iconImage); // Establecemos el icono de la ventana
@@ -57,134 +63,134 @@ public class Principal extends JFrame {
 
         URL urlLogo2 = Proyecto.Principal.class.getResource("Imagenes/gibson.png");
         ImageIcon imagen2 = new ImageIcon(urlLogo2);
-        Icon icono2 = new ImageIcon(imagen2.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+        Icon icono2 = new ImageIcon(imagen2.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
 
         URL urlLogo3 = Proyecto.Principal.class.getResource("Imagenes/fender_logo1.png");
         ImageIcon imagen3 = new ImageIcon(urlLogo3);
-        Icon icono3 = new ImageIcon(imagen3.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+        Icon icono3 = new ImageIcon(imagen3.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
 
         URL urlLogo4 = Proyecto.Principal.class.getResource("Imagenes/original.png");
         ImageIcon imagen4 = new ImageIcon(urlLogo4);
-        Icon icono4 = new ImageIcon(imagen4.getImage().getScaledInstance(420, 300, Image.SCALE_SMOOTH));
+        Icon icono4 = new ImageIcon(imagen4.getImage().getScaledInstance(320, 240, Image.SCALE_SMOOTH));
 
         URL urlLogo5 = Proyecto.Principal.class.getResource("Imagenes/EVH-striped-logo.png");
         ImageIcon imagen5 = new ImageIcon(urlLogo5);
-        Icon icono5 = new ImageIcon(imagen5.getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH));
+        Icon icono5 = new ImageIcon(imagen5.getImage().getScaledInstance(150, 70, Image.SCALE_SMOOTH));
 
         URL urlLogo6 = Proyecto.Principal.class.getResource("Imagenes/39.jpg");
         ImageIcon imagen6 = new ImageIcon(urlLogo6);
-        Icon icono6 = new ImageIcon(imagen6.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH));
+        Icon icono6 = new ImageIcon(imagen6.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
 
         URL urlLogo7 = Proyecto.Principal.class.getResource("Imagenes/logo-amp-marshall-black-300px.png");
         ImageIcon imagen7 = new ImageIcon(urlLogo7);
-        Icon icono7 = new ImageIcon(imagen7.getImage().getScaledInstance(250, 125, Image.SCALE_SMOOTH));
+        Icon icono7 = new ImageIcon(imagen7.getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH));
 
         URL urlLogo8 = Proyecto.Principal.class.getResource("Imagenes/orange-amplifiers.png");
         ImageIcon imagen8 = new ImageIcon(urlLogo8);
-        Icon icono8 = new ImageIcon(imagen8.getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH));
+        Icon icono8 = new ImageIcon(imagen8.getImage().getScaledInstance(160, 160, Image.SCALE_SMOOTH));
 
         URL urlLogo9 = Proyecto.Principal.class.getResource("Imagenes/2560px-Jackson_guitars_logo.svg.png");
         ImageIcon imagen9 = new ImageIcon(urlLogo9);
-        Icon icono9 = new ImageIcon(imagen9.getImage().getScaledInstance(250, 75, Image.SCALE_SMOOTH));
+        Icon icono9 = new ImageIcon(imagen9.getImage().getScaledInstance(200, 60, Image.SCALE_SMOOTH));
 
         URL urlLogo10 = Proyecto.Principal.class.getResource("Imagenes/600dadd6516067000439acaa.png");
         ImageIcon imagen10 = new ImageIcon(urlLogo10);
-        Icon icono10 = new ImageIcon(imagen10.getImage().getScaledInstance(250, 80, Image.SCALE_SMOOTH));
+        Icon icono10 = new ImageIcon(imagen10.getImage().getScaledInstance(180, 60, Image.SCALE_SMOOTH));
 
         URL urlLogo11 = Proyecto.Principal.class.getResource("Imagenes/MXR_Logo_G.png");
         ImageIcon imagen11 = new ImageIcon(urlLogo11);
-        Icon icono11 = new ImageIcon(imagen11.getImage().getScaledInstance(255, 100, Image.SCALE_SMOOTH));
+        Icon icono11 = new ImageIcon(imagen11.getImage().getScaledInstance(200, 60, Image.SCALE_SMOOTH));
 
         URL urlLogo12 = Proyecto.Principal.class.getResource("Imagenes/Vox-logo-Warranty-DAR-WEB.png");
         ImageIcon imagen12 = new ImageIcon(urlLogo12);
-        Icon icono12 = new ImageIcon(imagen12.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH));
+        Icon icono12 = new ImageIcon(imagen12.getImage().getScaledInstance(140, 145, Image.SCALE_SMOOTH));
 
         URL urlLogo13 = Proyecto.Principal.class.getResource("Imagenes/Yamaha_logo_dark.svg.png");
         ImageIcon imagen13 = new ImageIcon(urlLogo13);
-        Icon icono13 = new ImageIcon(imagen13.getImage().getScaledInstance(240, 50, Image.SCALE_SMOOTH));
+        Icon icono13 = new ImageIcon(imagen13.getImage().getScaledInstance(180, 40, Image.SCALE_SMOOTH));
 
         //LABELS PARA LAS IMÁGENES Y TEXTOS
 
         JLabel imagenLogo = new JLabel();
         imagenLogo.setIcon(icono);
-        imagenLogo.setBounds(600, 5, 300, 140);
+        imagenLogo.setBounds(477, 5, 300, 140);
         panelPrincipal.add(imagenLogo);
 
         JLabel imagenLogo2 = new JLabel();
         imagenLogo2.setIcon(icono2);
-        imagenLogo2.setBounds(350, 230, 300, 150);
+        imagenLogo2.setBounds(255, 225, 300, 150);
         panelPrincipal.add(imagenLogo2);
 
         JLabel imagenLogo3 = new JLabel();
         imagenLogo3.setIcon(icono3);
-        imagenLogo3.setBounds(650, 220, 300, 150);
+        imagenLogo3.setBounds(556, 215, 300, 150);
         panelPrincipal.add(imagenLogo3);
 
         JLabel imagenLogo4 = new JLabel();
         imagenLogo4.setIcon(icono4);
-        imagenLogo4.setBounds(830, 240, 300, 150);
+        imagenLogo4.setBounds(748, 230, 300, 150);
         panelPrincipal.add(imagenLogo4);
 
         JLabel imagenLogo5 = new JLabel();
         imagenLogo5.setIcon(icono5);
-        imagenLogo5.setBounds(650, 370, 300, 150);
+        imagenLogo5.setBounds(555, 330, 300, 150);
         panelPrincipal.add(imagenLogo5);
 
         JLabel imagenLogo6 = new JLabel();
         imagenLogo6.setIcon(icono6);
-        imagenLogo6.setBounds(900, 375, 300, 150);
+        imagenLogo6.setBounds(820, 333, 300, 150);
         panelPrincipal.add(imagenLogo6);
 
         JLabel imagenLogo7 = new JLabel();
         imagenLogo7.setIcon(icono7);
-        imagenLogo7.setBounds(325, 370, 300, 150);
+        imagenLogo7.setBounds(225, 335, 300, 150);
         panelPrincipal.add(imagenLogo7);
 
         JLabel imagenLogo8 = new JLabel();
         imagenLogo8.setIcon(icono8);
-        imagenLogo8.setBounds(330, 530, 300, 150);
+        imagenLogo8.setBounds(250, 450, 300, 150);
         panelPrincipal.add(imagenLogo8);
 
         JLabel imagenLogo9 = new JLabel();
         imagenLogo9.setIcon(icono9);
-        imagenLogo9.setBounds(630, 520, 300, 150);
+        imagenLogo9.setBounds(535, 440, 300, 150);
         panelPrincipal.add(imagenLogo9);
 
         JLabel imagenLogo10 = new JLabel();
         imagenLogo10.setIcon(icono10);
-        imagenLogo10.setBounds(925, 526, 300, 150);
+        imagenLogo10.setBounds(830, 442, 300, 150);
         panelPrincipal.add(imagenLogo10);
 
         JLabel imagenLogo11 = new JLabel();
         imagenLogo11.setIcon(icono11);
-        imagenLogo11.setBounds(328, 665, 300, 150);
+        imagenLogo11.setBounds(230, 556, 300, 150);
         panelPrincipal.add(imagenLogo11);
 
         JLabel imagenLogo12 = new JLabel();
         imagenLogo12.setIcon(icono12);
-        imagenLogo12.setBounds(655, 695, 300, 150);
+        imagenLogo12.setBounds(562, 576, 300, 150);
         panelPrincipal.add(imagenLogo12);
 
         JLabel imagenLogo13 = new JLabel();
         imagenLogo13.setIcon(icono13);
-        imagenLogo13.setBounds(930, 670, 300, 150);
+        imagenLogo13.setBounds(831, 555, 300, 150);
         panelPrincipal.add(imagenLogo13);
 
         JLabel music = new JLabel("MUSIC");
-        music.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,120));
-        music.setBounds(220, 25, 400, 100);
+        music.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,100));
+        music.setBounds(160, 25, 400, 100);
         music.setForeground(new Color(0x049B04));
         panelPrincipal.add(music);
 
         JLabel store = new JLabel("STORE");
-        store.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,120));
-        store.setBounds(900, 25, 400, 100);
+        store.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE,100));
+        store.setBounds(775, 25, 400, 100);
         store.setForeground(new Color(0x049B04));
         panelPrincipal.add(store);
 
         JLabel nuestrasMarcas = new JLabel("NUESTRAS MARCAS:");
         nuestrasMarcas.setFont(new Font("Impact", Font.ROMAN_BASELINE,20));
-        nuestrasMarcas.setBounds(673, 165, 200, 100);
+        nuestrasMarcas.setBounds(552, 165, 200, 100);
         nuestrasMarcas.setForeground(Color.BLACK);
         panelPrincipal.add(nuestrasMarcas);
     }
@@ -196,7 +202,7 @@ public class Principal extends JFrame {
         navbar = new JMenuBar(); //Creación de la barra del menú
         navbar.setOpaque(true); //La hacemos opaca para aplicarle color de fondo y que así se pueda visualizar
         navbar.setBackground(Color.BLACK); //Le aplicamos color de fondo
-        navbar.setBounds(4, 145, 1528, 50); //Tamaño y posición del MenuBar
+        navbar.setBounds(4, 145, 1518, 50); //Tamaño y posición del MenuBar
         panelPrincipal.add(navbar); //La añadimos al PANEL
 
         // OPCIONES DEL MENU BAR (MENÚ, CONTACTO, INFO)
